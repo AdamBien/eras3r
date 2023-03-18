fcbcc20f1007faec0b29b5fedeef93ca9b632c42
Adam Bien <abien@adam-bien.com>
2023-03-18 07:32:23 +0100

    deletemarkers removal

--- eras3r/src/main/java/airhacks/eras3r/control/Logging.java
+++ eras3r/src/main/java/airhacks/eras3r/control/Logging.java
@@ -4,4 +4,9 @@ public interface Logging {
     static void log(String message) {
         System.out.println(message);
     }
+
+    static void log(Object message) {
+        System.out.println(message);
+    }
+
 }
--- eras3r/src/main/java/airhacks/eras3r/control/ObjectsRemover.java
+++ eras3r/src/main/java/airhacks/eras3r/control/ObjectsRemover.java
@@ -1,39 +1,53 @@
 package airhacks.eras3r.control;
 
-import airhacks.eras3r.boundary.BucketEraser;
+import java.util.List;
+
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Delete;
+import software.amazon.awssdk.services.s3.model.DeleteMarkerEntry;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
 import software.amazon.awssdk.services.s3.model.DeletedObject;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectVersionsRequest;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.ObjectVersion;
 
 public interface ObjectsRemover {
 
     static void eraseBucketContents(S3Client client, String bucketName, boolean deleteBucket) {
         Logging.log("deleting contents and %s bucket?: %b".formatted(bucketName, deleteBucket));
-        var listRequest = ListObjectsV2Request
+        var listRequest = ListObjectVersionsRequest
                 .builder()
                 .bucket(bucketName)
                 .build();
-        var s3Objects = client
-                .listObjectsV2(listRequest)
-                .contents();
-
-        var s3Keys = s3Objects
+        var listObjectResponse = client.listObjectVersions(listRequest);
+        var s3Keys = listObjectResponse
+                .versions()
                 .stream()
                 .limit(1000)
-                .map(S3Object::key)
-                .peek(Logging::log)
                 .map(ObjectsRemover::toIdentifier)
                 .toList();
-                
+
         if (s3Keys.isEmpty()) {
             System.out.println("bucket %s is empty".formatted(bucketName));
-            return;
+        } else {
+            deleteObjects(client, bucketName, s3Keys);
         }
 
+        s3Keys = listObjectResponse.deleteMarkers()
+                .stream()
+                .map(ObjectsRemover::toIdentifier)
+                .peek(Logging::log)
+                .toList();
+
+        if (s3Keys.isEmpty()) {
+            System.out.println("no delete markers are available in bucket: %s".formatted(bucketName));
+        } else {
+            deleteObjects(client, bucketName, s3Keys);
+        }
+
+    }
+
+    private static void deleteObjects(S3Client client, String bucketName, List<ObjectIdentifier> s3Keys) {
         var delete = Delete.builder()
                 .objects(s3Keys)
                 .build();
@@ -51,11 +65,20 @@ public interface ObjectsRemover {
                 .stream()
                 .map(DeletedObject::key)
                 .forEach(Logging::log);
+
     }
 
-    private static ObjectIdentifier toIdentifier(String objectName) {
+    private static ObjectIdentifier toIdentifier(ObjectVersion version) {
         return ObjectIdentifier.builder()
-                .key(objectName)
+                .key(version.key())
+                .versionId(version.versionId())
+                .build();
+    }
+
+    private static ObjectIdentifier toIdentifier(DeleteMarkerEntry version) {
+        return ObjectIdentifier.builder()
+                .key(version.key())
+                .versionId(version.versionId())
                 .build();
     }
 
